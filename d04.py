93ead6d4ba432e8893621ff1f01c754a20f5166a
5loika <allenn@gmail.com>
2022-12-04 21:06:47 +0000

    Rewrite Day 04 to use pandas Intervals

--- d04.py
+++ d04.py
@@ -1,19 +1,29 @@
 import sys
 from aocd import data
 from aocd import submit
+import re
+import pandas as pd
+
+testdata = '''2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8'''
+test1 = 2
+test2 = 4
 
 def part1(mydata):
   count = 0
   lines = mydata.splitlines()
   for line in lines:
-    a,b = line.split(',')
-    a1,a2 = a.split('-')
-    b1,b2 = b.split('-')
-    if int(a1) <= int(b1) and int(a2) >= int(b2):
-      # b is contained in a
-      count += 1
-    elif int(b1) <= int(a1) and int(b2) >= int(a2):
-      # a is contained in b
+    # use re-findall to extract numbers from line
+    a1,a2,b1,b2 = [int(x) for x in re.findall(r'\d+', line)]
+    # use pandas Interval to represent each range
+    a = pd.Interval(a1,a2,closed="both")
+    b = pd.Interval(b1,b2,closed="both")
+    # check if either Interval is fully contained by the other
+    if a in b or b in a:
       count += 1
   return(count)
 
@@ -21,38 +31,29 @@ def part2(mydata):
   count = 0
   lines = mydata.splitlines()
   for line in lines:
-    a,b = line.split(',')
-    a1,a2 = a.split('-')
-    b1,b2 = b.split('-')
-    if int(a1) <= int(b1) and int(b1) <= int(a2):
-      # b starts in a
+    # use re-findall to extract numbers from line
+    a1,a2,b1,b2 = [int(x) for x in re.findall(r'\d+', line)]
+    # use pandas Interval to represent each range
+    a = pd.Interval(a1,a2,closed="both")
+    b = pd.Interval(b1,b2,closed="both")
+    # check if the intervals overlap
+    if a.overlaps(b):
       count += 1
-    elif int(b1) <= int(a1) and int(a1) <= int(b2):
-      # a starts in b
-      count += 1
-    elif int(a2) >= int(b1) and int(a2) <= int(b2):
-      # a ends in b
-      count += 1
-    elif int(b2) >= int(a1) and int(b2) <= int(a2):
-      # b ends in a
-      count+= 1
   return(count)
   
 if __name__ == "__main__":
-  expect1 = None
-  expect2 = None
   if len(sys.argv) > 1:
     if sys.argv[1] == 'test':
-      with open(__file__.replace('.py','_test.txt'), "r") as f:
-        test = f.read()
-      if len(sys.argv) > 2:
-        expect1 = sys.argv[2]
-      if len(sys.argv) > 3:
-        expect2 = sys.argv[3]
-      test1 = part1(test)
-      print('Test 1 - Expected: ',expect1,'\tReturned: ', test1)
-      test2 = part2(test)
-      print('Test 2 - Expected: ',expect2,'\tReturned: ', test2)
+      result1 = part1(testdata)
+      if test1 == result1:
+        print('Test 1 - Pass:', result1)
+      else:
+        print('Test 1 - Fail:', result1)
+      result2 = part2(testdata)
+      if test2 == result2:
+        print('Test 2 - Pass:', result2)
+      else:
+        print('Test 2 - Fail:', result2)
     elif sys.argv[1] == 'submit':
       if sys.argv[2] == 'a':
         submit(part1(data))
