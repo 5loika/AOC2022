dffe9bc59c58c41734c3bcc7948ea98996eeb97b
5loika <allenn@gmail.com>
2022-12-09 00:24:27 +0000

    Day 8 - working

--- d08.py
+++ d08.py
@@ -22,13 +22,13 @@ def part1(mydata):
   trees = np.array(txtTrees)
   rows, cols = trees.shape
   count = 2*(rows-1)+2*(cols-1) # Count all edge trees
-  for r in range(1,rows-1): # Check all interior trees
-    for c in range(1,cols-1):
-      t = trees[r,c]
-      left = max(trees[r,:c])
-      right = max(trees[r,c+1:])
-      top = max(trees[:r,c])
-      bottom = max(trees[r+1:,c])
+  for row in range(1,rows-1): # Check all interior trees
+    for col in range(1,cols-1):
+      t = trees[row,col]
+      left = max(trees[row,:col])
+      right = max(trees[row,col+1:])
+      top = max(trees[:row,col])
+      bottom = max(trees[row+1:,col])
       if t>left or t>right or t>top or t>bottom:
         count += 1
   return(count)
@@ -37,41 +37,34 @@ def part2(mydata):
     txtTrees = []
     scores = []
     for l in mydata.splitlines():
-        txtTrees.append(list([int(x) for x in l]))
+      txtTrees.append(list([int(x) for x in l]))
     trees = np.array(txtTrees)
     rows, cols = trees.shape
     for row in range(1,rows-1): # Check all interior trees
-        for col in range(1,cols-1):
-            t = trees[row,col]
-            left = trees[row,:col]
-            right = trees[row,col+1:]
-            up = trees[:row,col]
-            down = trees[row+1:,col]
-            l = 0
-            r = 0
-            u = 0
-            d = 0
-            if len(left) > 0: # Not left edge
-                for n in left[::-1]:
-                    l += 1
-                    if n >= t:
-                        break
-            if len(up) > 0: # Not top edge
-                for n in up[::-1]:
-                    u += 1
-                    if n >= t:
-                        break
-            if len(right) > 0: # Not right edge
-                for n in right:
-                    r += 1
-                    if n >= t:
-                        break
-            if len(down) > 0: # not bottom edge
-                for n in down:
-                    d += 1
-                    if n >= t:
-                        break
-            scores.append(l * u * r * d)
+      for col in range(1,cols-1):
+        t = trees[row,col]
+        left = trees[row,:col]
+        right = trees[row,col+1:]
+        up = trees[:row,col]
+        down = trees[row+1:,col]
+        l = r = u = d = 0
+        for n in left[::-1]:
+          l += 1
+          if n >= t:
+            break
+        for n in up[::-1]:
+            u += 1
+            if n >= t:
+              break
+        for n in right:
+            r += 1
+            if n >= t:
+              break
+        for n in down:
+            d += 1
+            if n >= t:
+              break
+        scores.append(l * u * r * d)
     return(max(scores))
 
 if __name__ == "__main__":
