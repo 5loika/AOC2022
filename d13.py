adc41728dafe004b5109814adfc30cc46232dd2b
5loika <allenn@gmail.com>
2022-12-15 02:34:36 +0000

    Day 13 - Incomplete

---
+++ d13.py
@@ -0,0 +1,77 @@
+#!/bin/python3
+
+import sys
+from aocd import data
+from aocd import submit
+
+testdata = '''[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]'''
+test1 = 13
+test2 = None
+
+def part1(mydata):
+  pairs = mydata.split('\n\n')
+  for p in pairs:
+    l,r = p.splitlines()
+    print(l,r)
+    NotImplemented  
+  return(None)
+
+def part2(mydata):
+  lines = mydata.splitlines()
+  for line in lines:
+    NotImplemented  
+  return(None)
+    
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    if sys.argv[1] == 'submit':
+      if sys.argv[2] == 'a':
+        submit(part1(data), part='a')
+      elif sys.argv[2] =='b':
+        submit(part2(data), part='b')
+      else:
+        print('Specify a or b') 
+    elif sys.argv[1] == 'run':
+      if sys.argv[2] == 'a':
+        print('Part 1: ',part1(data))
+      elif sys.argv[2] =='b':
+        print('Part 2: ',part2(data))
+      else:
+        print('Part 1: ',part1(data))
+        print('Part 2: ',part2(data))
+    elif sys.argv[1] == 'test':
+      result1 = part1(testdata)
+      if test1 == result1:
+        print('Test 1 - Pass:', result1)
+      else:
+        print('Test 1 - Fail:', result1)
+      result2 = part2(testdata)
+      if test2 == result2:
+        print('Test 2 - Pass:', result2)
+      else:
+        print('Test 2 - Fail:', result2)
+  else:
+    print('Part 1: ',part1(data))
+    print('Part 2: ',part2(data))
